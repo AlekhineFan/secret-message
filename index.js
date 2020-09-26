b97905dde868d2f48405ca17a657a2d3b10b6b2a
Mark Szavin <szavinm@yahoo.com>
2020-09-26 10:51:14 +0200

    encode message

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+document.querySelector("form").addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  document.querySelector("#link-input").value = encrypted;
+});
